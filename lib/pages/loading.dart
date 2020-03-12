ce08b2965c6ae19ca6fc0cfb453aa5c9ceb89e1c
Eprel Acer ICTO <apr.medina@gmail.com>
2020-03-12 13:47:21 +0800

    Added http package and async await

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +8,24 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    // get
+    Response response =
+        await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(response.body);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Loading Screen')
-    );
+    return Scaffold(body: Text('Loading Screen'));
   }
 }
