6c7835b5c9e9139fa70fe69c687d4d20deb0f7dc
iameprel <apr.medina@gmail.com>
2020-10-15 23:34:27 +0800

    Update loading.dart

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -29,7 +29,7 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[900],
+      backgroundColor: Colors.blue[700],
       body: Center(
         child: SpinKitFadingCube(
           color: Colors.white,
